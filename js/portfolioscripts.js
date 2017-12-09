ea6fb26884999b53bc67ae022899c9813d234c34
Kira Loo <youhavebeenkiraed@gmail.com>
2017-12-09 14:34:44 -0800

    portfolio fix

--- js/portfolioscripts.js
+++ js/portfolioscripts.js
@@ -1,170 +1,103 @@
-window.onload = function(){
-      var overlayDiv;
-      var imgBox;
-      var prevButt;
-      var nextButt;
-      var sliderBox;
-      var closeButt;
-      var count;
-      var images;
-      var imgDivs;
-      var imgObjectArray;
-      var thumbCount;
+! function ($) {
+     var defaults = {
+         sectionContainer: "> section",
+         angle: 50,
+         opacity: true,
+         scale: true,
+         outAnimation: true,
+         pageContainer: '.page_container',
+         pageOpacity: true
+     };
+     $.fn.tiltedpage_scroll = function (options) {
+         var settings = $.extend({}, defaults, options),
+             el = $(this);
+         el.find(settings.sectionContainer).addClass("tps-section");
+         el.find('.tps-section').each(function () {
+             var el2 = $(this);
+             el2.wrapInner("<div class='tps-wrapper'></div>");
+         });
 
-    imgObjectArray = [];
-    thumbCount = 18;
+         function isElementInViewport(el3) {
+             var docViewTop = $(window).scrollTop(),
+                 docViewBottom = docViewTop + $(window).height(),
+                 elemTop = el3.offset().top,
+                 elemBottom = elemTop + el3.outerHeight(true);
+             return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom));
+         }
 
-  var createThumb = function(imgSource){
-          this.imgSource = imgSource;
-          imgObjectArray.push(imgSource);
-      };
+         function elementVisibilityMayChange(el4) {
+             if (isElementInViewport(el4)) {
+                 el4.addClass("tps-inview")
+             } else {
+                 el4.removeClass("tps-inview")
+             }
+         }
+         $(window).on('DOMContentLoaded load resize scroll', function () {
+             el.find(settings.sectionContainer).each(function () {
+                 elementVisibilityMayChange($(this));
+             });
+             el.find('.tps-section.tps-inview > .tps-wrapper').each(function (index) {
+                 var el2 = $(this),
+                     elc = el2.find(settings.pageContainer),
+                     opacity = 0,
+                     opacity2 = 0,
+                     st = $(window).scrollTop(),
+                     deg = ((el2.parent().offset().top - el2.parent().height()) - st) / $(window).height() * (settings.angle * 3),
+                     scale = ((st + $(window).height() - (el2.parent().offset().top - el2.parent().height())) / ($(window).height()));
+                 if (scale > 1) scale = 1;
+                 if (deg < 0) deg = 0;
+                 if (st > el2.parent().offset().top) {
+                     if (settings.outAnimation == false) {
+                         opacity = 1;
+                         opacity2 = 1;
+                         if (opacity < 0) {
+                           opacity = 0;
+                           opacity2 = 0;
+                         }
+                         if (deg < 0) deg = 0;
+                     } else {
+                         opacity = ((el2.parent().offset().top + ($(window).height() * 1.2) - st)) / ($(window).height());
+                         opacity2 = opacity;
+                         opacity = Math.pow(opacity, 25);
+                         opacity2 = Math.pow(opacity2, 25);
+                         //console.log('- '+opacity2);
+                         deg = (el2.parent().offset().top - st) / $(window).height() * (settings.angle * 3);
+                         scale = ((st + $(window).height() - el2.parent().offset().top) / ($(window).height()));
+                     }
+                 } else {
+                     if (index != 0) {
+                         opacity = ((st + $(window).height() - el2.parent().offset().top + (el2.height() / 2)) / $(window).height());
+                       opacity2 = opacity / 2;
+                       opacity2 = opacity2 < 0.4 ? opacity2/2 : opacity2;
+                         //console.log(opacity2);
+                         if (opacity > 1) {
+                             opacity = 1;
+                             opacity2 = 1;
+                         }
+                     } else {
+                         opacity = 1;
+                         opacity2 = 1;
+                         deg = 0;
+                         scale = 1;
+                     }
+                 } if (settings.scale == false) scale = 1;
+                 if (settings.angle == false) deg = 0;
+                 if (settings.opacity == false) {
+                   opacity = 1;
+                   opacity2 = 1;
+                 }
+                 el2.css({
+                     'transform': 'rotateX(' + deg + 'deg) scale(' + scale + ', ' + scale + ')',
+                     opacity: opacity
+                 });
+                 elc.css({opacity: opacity2});
+             });
+         });
+     }
+ }(window.jQuery);
 
-     img = new createThumb('url(img/slide4.png)');
-     img = new createThumb('url(img/slide5.png)');
-     img = new createThumb('url(img/slide6.png)');
-     img = new createThumb('url(img/slide1.png)');
-     img = new createThumb('url(img/slide2.png)');
-     img = new createThumb('url(img/slide3.png)');
-     img = new createThumb('url(img/slide10.png)');
-     img = new createThumb('url(img/tomatoholder.png)');
-     img = new createThumb('url(img/tomatoholder.png)');
-     img = new createThumb('url(img/slide7.png)');
-     img = new createThumb('url(img/slide8.png)');
-     img = new createThumb('url(img/slide9.png)');
-     img = new createThumb('url(img/slide11.png)');
-     img = new createThumb('url(img/tomatoholder.png)');
-     img = new createThumb('url(img/tomatoholder.png)');
-     img = new createThumb('url(img/tomatoholder.png)');
-     img = new createThumb('url(img/tomatoholder.png)');
-     img = new createThumb('url(img/tomatoholder.png)');
-
-    var header = document.createElement("DIV");
-        header.className = "header";
-    var headerText = document.createTextNode("Portfolio");
-        header.appendChild(headerText);
-        document.body.appendChild(header);
-
-    var container = document.createElement("DIV");
-        container.className = "container";
-        document.body.appendChild(container);
-
-    var thumbnailBody = document.createElement("DIV");
-        thumbnailBody.className = "thumbnailBody";
-        container.appendChild(thumbnailBody);
-
-      for (var i = 0; i < thumbCount; i++) {
-
-              imgDivs = document.createElement("DIV");
-              imgDivs.className = "imgBox";
-              imgDivs.style.background = imgObjectArray[i];
-              imgDivs.style.backgroundSize = "cover";
-
-              imgDivs.onclick = function(){
-
-                createPopup();
-
-                  container.classList.add("blur");
-
-                  imgBox = document.createElement("DIV");
-                  imgBox.className = "popUpImg";
-
-                  var currentImage = this.style.backgroundImage;
-                      imgBox.style.backgroundImage = currentImage;
-
-                  imgBox.style.backgroundSize = "cover";
-                  sliderBox.appendChild(imgBox);
-
-            for (var j = 0; j < thumbCount; j++) {
-
-
-                      if (currentImage === imgObjectArray[j] || currentImage.replace(/["]/g, '') === imgObjectArray[j]) {
-
-                          return count = j;
-                      }
-                  }
-              }
-
-              thumbnailBody.appendChild(imgDivs);
-
-            }
-
-function createPopup(){
-
-            sliderBox = document.createElement("DIV");
-            sliderBox.className = "sliderBox";
-            document.body.appendChild(sliderBox);
-
-            overlayDiv = document.createElement("DIV");
-            overlayDiv.className = "overlay";
-            overlayDiv.onclick = function(){
-            closeOverlay();
-          };
-
-            document.body.appendChild(overlayDiv);
-
-            closeButt = document.createElement("DIV");
-        var closeText = document.createTextNode("Close");
-            closeButt.appendChild(closeText);
-            closeButt.className = "closeButt";
-            closeButt.onclick = closeOverlay;
-            overlayDiv.appendChild(closeButt);
-
-            prevButt = document.createElement("DIV");
-            prevButt.className = "prevButt";
-            prevButt.id = "navButt";
-            prevButt.onclick = prevImage;
-            sliderBox.appendChild(prevButt);
-
-            nextButt = document.createElement("DIV");
-            nextButt.className = "nextButt";
-            nextButt.id = "navButt";
-            nextButt.onclick = nextImage;
-            sliderBox.appendChild(nextButt);
-}
-
-      function nextImage() {
-
-        count++;
-
-        imgBox.style.background = imgObjectArray[count];
-        imgBox.style.backgroundSize = "cover";
-
-        if (count >= imgObjectArray.length - 1) {
-          count = -1;
-        }
-      }
-
-      function prevImage() {
-
-        count--;
-
-        if (count < 0) {
-          count = imgObjectArray.length - 1;
-        }
-
-        imgBox.style.background = imgObjectArray[count];
-        imgBox.style.backgroundSize = "cover";
-      }
-
-    function closeOverlay(){
-      overlayDiv.style.display = "none";
-      sliderBox.style.display = "none";
-      container.classList.remove("blur");
-    }
-
-
-    document.onkeydown = function(e) {
-        switch (e.keyCode) {
-            case 37:
-                prevImage();
-                break;
-            case 39:
-                nextImage();
-                break;
-            case 27:
-                closeOverlay();
-                break;
-
-        };
-    };
-};
+$(document).ready(function(){
+  $(".main").tiltedpage_scroll({
+    angle: 20
+  });
+});
